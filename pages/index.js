b84199d57063747159e991fc667919de94b3edbe
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-12-19 15:59:55 +0100

    Change: hero title

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Fade from 'react-reveal/Fade';
 import Bounce from 'react-reveal/Bounce';
 import Slide from 'react-reveal/Slide';
+import Projects from '../components/Projects';
 
 export default function Home() {
   return (
@@ -12,17 +13,24 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       {/* <Navbar /> */}
-      <div class="hero">
+      <div class="hero" id="home_section">
         <div>
           <Fade left>
             <h1 className="title">Jarno <span className="purple">Bogaert.</span></h1>
           </Fade>
           <Fade right>
-            <p className="subtitle">Full stack <span className="underline">software</span> engineer.</p>
+            <p className="subtitle">Full stack <span className="underline">software</span> developer.</p>
           </Fade>
 
         </div>
       </div>
+      {/* <div className="container">
+        <section id="projects_section">
+          <h1>Projects.</h1>
+          <br />
+          <Projects />
+        </section>
+      </div> */}
       {/* <Navbar />
       <div className="container">
         <section id="home_section" className="section">
